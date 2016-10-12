3ad75432e19a639bc34ce4c8ba82df67014ed3d0
Alok Mysore <alok.shankar.m@gmail.com>
2016-10-11 19:03:51 -0700

    commit at Tue Oct 11 19:03:51 PDT 2016

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-printf "\n*** Begin: commit source ***\n"
+printf "\n*** Begin: commit source ***\n\n"
 
 git add .
 git commit -m "commit at $(date)"
 git push
 
-printf "\n*** End: commit source, Begin: build ***\n"
+printf "\n*** End: commit source, Begin: build ***\n\n"
 
 cd _site/
 jekyll build
 
-printf "\n*** End: build, Begin: Commit website ***\n"
+printf "\n*** End: build, Begin: Commit website ***\n\n"
 
 git add .
 git commit -m "commit at $(date)"
 git push  origin master
 
-printf "\n*** End: Commit website ***\n"
+printf "\n*** End: Commit website ***\n\n"
